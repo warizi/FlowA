c1341fb0e68c5aa5296116f9f4ee57b5b72a4795
Jin SeungHyeon <jinsh9050@gmail.com>
2025-05-13 08:36:57 +0900

    common button 추가

---
+++ src/shared/components/ui/button/CommonButton.tsx
@@ -0,0 +1,48 @@
+/** @jsxImportSource @emotion/react */
+
+import { useTheme, type Theme } from "@emotion/react";
+
+type CommonButtonProps = {
+  onClick?: () => void;
+  children?: React.ReactNode;
+  color?: string;
+  padding?: string;
+  backgroundColor?: string;
+  margin?: string;
+}
+
+const buttonStyle = (theme: Theme) => ({
+  border: "none",
+  borderRadius: theme.radius.medium,
+  cursor: "pointer",
+})
+
+function CommonButton({
+  onClick = () => {},
+  children,
+  color = "inherit",
+  padding = "5px",
+  backgroundColor = "inherit",
+  margin = "0",
+  ...rest
+}: CommonButtonProps) {
+  const theme = useTheme();
+
+  return (
+    <button
+      css={{
+        ...buttonStyle(theme), 
+        color, 
+        padding, 
+        backgroundColor, 
+        margin
+      }}
+      onClick={onClick}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default CommonButton;
