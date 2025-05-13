e1b26cb25d235821edbd7e350b109a5ea9aebf04
Jin SeungHyeon <jinsh9050@gmail.com>
2025-05-14 08:29:28 +0900

    shared > const > route_url

---
+++ src/shared/constants/route/ROUTE_URL.ts
@@ -0,0 +1,14 @@
+export const ROUTE_URL = {
+  HOME: "/home",
+  TODO: "/todo",
+  TODO_CATE: "/todo/cate",
+  TODO_TODAY: "/todo/today",
+  TODO_IMPORTANT: "/todo/important",
+  TODO_ALL: "/todo/all",
+  NOTE: "/note",
+  NOTE_CATE: "/note/cate",
+  NOTE_IMPORTANT: "/note/important",
+  NOTE_RECENT: "/note/recent",
+  NOTE_ALL: "/note/all",
+  TEST: "/test",
+};
