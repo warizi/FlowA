9c14107f063baeedd07d995798855d08d1b9dccb
Jin SeungHyeon <jinsh9050@gmail.com>
2025-05-14 08:27:36 +0900

    icon 추가

---
+++ src/shared/components/icon/index.ts
@@ -0,0 +1,49 @@
+import ExitIcon from "./ui/ExitIcon";
+import HomeIcon from "./ui/HomeIcon";
+import TaskIcon from "./ui/TaskIcon";
+import MonitorIcon from "./ui/MonitorIcon";
+import ArrowRightIcon from "./ui/ArrowRightIcon";
+import PlusIcon from "./ui/PlusIcon";
+import FileCheckIcon from "./ui/FileCheckIcon";
+import SunIcon from "./ui/SunIcon";
+import StarIcon from "./ui/StarIcon";
+import CheckIcon from "./ui/CheckIcon";
+import TrashIcon from "./ui/TrashIcon";
+import ArrowDown from "./ui/ArrowDown";
+import FolderIcon from "./ui/FolderIcon";
+import SidebarIcon from "./ui/SidebarIcon";
+import SearchIcon from "./ui/SearchIcon";
+import CloseIcon from "./ui/CloseIcon";
+import NoteIcon from "./ui/NoteIcon";
+import ListIcon from "./ui/ListIcon";
+import GridIcon from "./ui/GridIcon";
+import BookMarkIcon from "./ui/BookMarkIcon";
+import MoonIcon from "./ui/MoonIcon";
+import CalendarIcon from "./ui/CalendarIcon";
+import StopWatchIcon from "./ui/StopWatchIcon";
+
+export {
+  ExitIcon,
+  HomeIcon,
+  TaskIcon,
+  MonitorIcon,
+  ArrowRightIcon,
+  PlusIcon,
+  FileCheckIcon,
+  SunIcon,
+  StarIcon,
+  CheckIcon,
+  TrashIcon,
+  ArrowDown,
+  FolderIcon,
+  SidebarIcon,
+  SearchIcon,
+  CloseIcon,
+  NoteIcon,
+  ListIcon,
+  GridIcon,
+  BookMarkIcon,
+  MoonIcon,
+  CalendarIcon,
+  StopWatchIcon,
+};
---
+++ src/shared/components/icon/model/type.ts
@@ -0,0 +1,5 @@
+export type IconProps = {
+  color?: string;
+  size?: number;
+  fill?: string;
+};
